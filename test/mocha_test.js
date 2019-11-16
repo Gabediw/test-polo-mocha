cfdd62a2ba67b877419837c870e729d7428a5d35
Gabriela W <gabediw@gmail.com>
2019-11-16 11:42:37 -0300

    Se muestra nombre de parámetro

--- test/mocha_test.js
+++ test/mocha_test.js
@@ -3,6 +3,7 @@ describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
       console.log(process.env.USUARIO)
+      console.log(process.env.NOMBRE)
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
