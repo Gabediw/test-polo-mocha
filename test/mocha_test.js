3d18e1414e7c197538e4f65534f6189ca9840c65
Gabriela W <gabediw@gmail.com>
2019-11-16 11:34:01 -0300

    Se realizaron cambios

--- test/mocha_test.js
+++ test/mocha_test.js
@@ -2,6 +2,7 @@ var assert = require('assert');
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
+      console.log(process.env.USUARIO)
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
